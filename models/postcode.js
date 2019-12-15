d48e4f8e0e13949d6d9f3842d5317ca3b3817caa
Dave <DaveBathnes@users.noreply.github.com>
2019-12-15 18:43:37 +0000

    #3 Making postcode searching case insensitive

--- models/postcode.js
+++ models/postcode.js
@@ -4,7 +4,7 @@ module.exports.getPostcode = async (postcode) => {
 	let postcode_data = {};
 	const query = 'select * from vw_postcodes where postcode_trimmed = replace($1, \' \', \'\')';
 	try {
-		const { rows } = await pool.query(query, [postcode]);
+		const { rows } = await pool.query(query, [postcode.toUpperCase()]);
 		if (rows && rows.length > 0) postcode_data = rows[0];
 	} catch (e) { }
 	return postcode_data;
@@ -14,7 +14,7 @@ module.exports.getPostcodeLsoasFromSectors = async (sectors) => {
 	let lsoas = {};
 	const query = 'select * from fn_postcodelsoasfromsectors($1::jsonb)';
 	try {
-		const { rows } = await pool.query(query, [JSON.stringify(sectors)]);
+		const { rows } = await pool.query(query, [JSON.stringify(sectors).toUpperCase()]);
 		if (rows && rows.length > 0) {
 			rows.forEach(row => {
 				lsoas[row.lsoa] = row.postcode;
