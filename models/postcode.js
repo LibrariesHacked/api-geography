63b248e7c74fc6969d555f67a78142b8bd3d3d1d
Dave <DaveBathnes@users.noreply.github.com>
2019-12-18 21:50:30 +0000

    #4 Minor performance improvements. Closes #4

--- models/postcode.js
+++ models/postcode.js
@@ -2,9 +2,9 @@ const pool = require('../helpers/database');
 
 module.exports.getPostcode = async (postcode) => {
 	let postcode_data = {};
-	const query = 'select * from vw_postcodes where postcode_trimmed = replace($1, \' \', \'\')';
+	const query = 'select * from vw_postcodes where postcode_trimmed = $1';
 	try {
-		const { rows } = await pool.query(query, [postcode.toUpperCase()]);
+		const { rows } = await pool.query(query, [postcode.replace(/\s/g, '').toUpperCase()]);
 		if (rows && rows.length > 0) postcode_data = rows[0];
 	} catch (e) { }
 	return postcode_data;
@@ -12,9 +12,9 @@ module.exports.getPostcode = async (postcode) => {
 
 module.exports.getPostcodeLsoasFromSectors = async (sectors) => {
 	let lsoas = {};
-	const query = 'select * from fn_postcodelsoasfromsectors($1::jsonb)';
+	const query = 'select * from fn_postcodelsoasfromsectors($1)';
 	try {
-		const { rows } = await pool.query(query, [JSON.stringify(sectors).toUpperCase()]);
+		const { rows } = await pool.query(query, [JSON.stringify(sectors).replace(/\s/g, '').toUpperCase()]);
 		if (rows && rows.length > 0) {
 			rows.forEach(row => {
 				lsoas[row.lsoa] = row.postcode;
