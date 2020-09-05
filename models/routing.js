f2ce97cb7d38a532c2b778d503f00678a4ff4215
Dave <DaveBathnes@users.noreply.github.com>
2020-09-05 22:22:04 +0100

    #9 Adding duration and interval to isochrone service

--- models/routing.js
+++ models/routing.js
@@ -1,14 +1,14 @@
 const axios = require('axios')
 
-module.exports.getIsochrone = async (transport, longitude, latitide, location = 'destination') => {
+module.exports.getIsochrone = async (transport, longitude, latitide, location = 'destination', duration = 1800, interval = 300) => {
   const url = 'https://api.openrouteservice.org/v2/isochrones/' + transport
   const body = {
     locations: [[longitude, latitide]],
     location_type: location,
     intersections: true,
     attributes: ['area', 'total_pop', 'reachfactor'],
-    range: [1800],
-    interval: 300
+    range: [duration],
+    interval: interval
   }
   const config = {
     headers: {
