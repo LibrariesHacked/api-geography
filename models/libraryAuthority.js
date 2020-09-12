3287262a70ad098d6798902f7de073df68d77018
Dave <DaveBathnes@users.noreply.github.com>
2020-09-12 21:26:05 +0100

    #10 Adjusting detail on geom precision

--- models/libraryAuthority.js
+++ models/libraryAuthority.js
@@ -3,7 +3,7 @@ const viewFields = ['utla19cd', 'utla19nm', 'utla19nmw']
 
 module.exports.getLibraryAuthorities = async () => {
   let authorities = []
-  const query = 'select ' + viewFields.join(', ') + ', st_asgeojson(st_snaptogrid(st_simplify(st_transform(geom, 4326), 0.01, false), 0.01)) as geom, st_asgeojson(st_transform(st_snaptogrid(bbox, 0.001), 4326)) as bbox from vw_library_boundaries'
+  const query = 'select ' + viewFields.join(', ') + ', st_asgeojson(st_snaptogrid(st_simplify(st_transform(geom, 4326), 0.001, false), 0.001)) as geom, st_asgeojson(st_transform(st_snaptogrid(bbox, 0.001), 4326)) as bbox from vw_library_boundaries'
   try {
     const { rows } = await pool.query(query)
     if (rows && rows.length > 0) authorities = rows
