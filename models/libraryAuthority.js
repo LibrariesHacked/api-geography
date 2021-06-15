01eecc117c99e0aa1b246e33395d70f46790d4af
Dave <8709533+DaveBathnes@users.noreply.github.com>
2021-06-15 23:49:22 +0100

    #14 Simplifying library authority boundaries

--- models/libraryAuthority.js
+++ models/libraryAuthority.js
@@ -3,7 +3,7 @@ const viewFields = ['utla19cd', 'utla19nm', 'utla19nmw', 'st_asgeojson(st_transf
 
 module.exports.getLibraryAuthorities = async () => {
   let authorities = []
-  const query = 'select ' + viewFields.join(', ') + ', st_asgeojson(st_snaptogrid(st_simplify(st_transform(geom, 4326), 0.001, false), 0.001)) as geom, st_asgeojson(st_transform(st_snaptogrid(bbox, 0.001), 4326)) as bbox from vw_library_boundaries'
+  const query = 'select ' + viewFields.join(', ') + ', st_asgeojson(st_simplify(st_transform(geom, 4326), 0.01, false)) as geom, st_asgeojson(st_transform(st_snaptogrid(bbox, 0.1), 4326)) as bbox from vw_library_boundaries'
   try {
     const { rows } = await pool.query(query)
     if (rows && rows.length > 0) authorities = rows
@@ -29,8 +29,8 @@ module.exports.getLibraryAuthorityByName = async (name) => {
   try {
     const { rows } = await pool.query(query, [name])
     if (rows && rows.length > 0) libraryAuthorityData = rows[0]
-  } catch (e) { 
-    console.log(e);
+  } catch (e) {
+    console.log(e)
   }
   return libraryAuthorityData
 }
